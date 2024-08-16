5f0f71a44d86751bfd04923decabb100c565ce05
Shahd Elhlwaney <ShahdElhlwaney@example.com>
2024-08-17 01:42:42 +0300

    Three Ways to implement singleton design pattern

---
+++ SingletonDP/Card.cs
@@ -0,0 +1,36 @@
+﻿namespace SingletonDP
+{
+    public class Card
+    {
+
+        // Singleton Design Pattern
+
+        /// 1- Using Private Constructor
+        private static Card instance;
+        //public int Data { get; set; }
+        //public static Card Instance
+        //{
+        //    get
+        //    {
+        //        if (instance == null)
+        //        {
+        //            instance = new Card();
+        //        }
+        //        return instance;
+        //    }
+        //}
+        private Card() { }
+
+        /// 2- Using Static Constructor (static constructor only runs once)
+
+        static Card() {
+            instance = new Card();
+        }
+        public static Card GetInstance()
+            => Instant;
+
+        /// 3- Using Static Readonly Property
+            public  readonly static Card Instant =new Card();
+
+    }
+}
---
+++ SingletonDP/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletonDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Card c1=Card.Instance;
+            //c1.Data = 20;
+            //Card c2 = Card.Instance;
+            //c2.Data = 10;
+            //Console.WriteLine(c1.GetHashCode());
+            //Console.WriteLine(c2.GetHashCode());
+            //Console.WriteLine("/////////////////////////////");
+            //Console.WriteLine("C1"+c1.Data);
+            //Console.WriteLine("C2" + c2.Data);
+
+            Card c1 = Card.GetInstance();
+            Card c2 = Card.GetInstance();
+            Console.WriteLine(c1.GetHashCode());
+            Console.WriteLine(c2.GetHashCode());
+
+        }
+    }
+}
